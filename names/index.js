97b6d04f77bf6178d37d314d0a344671b408ee5f
Varun Vaggu <varunvaggu@gmail.com>
2024-07-09 01:54:26 +0530

    Add files via upload

---
+++ names/index.js
@@ -0,0 +1,10 @@
+const peopleNames = require('../country/state/city/index.js')
+const getFirstNames = require('../utilities/utils/index.js')
+
+const getPeopleInCity = peopleNames => {
+  return getFirstNames(peopleNames)
+}
+
+//console.log(getPeopleInCity())
+
+module.exports = getPeopleInCity
